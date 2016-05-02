06655e7ba4377fbd4cebdbaad96c782bce60dfda
seven8527 <quanbowei007@yahoo.com>
2016-05-02 20:03:47 +0800

    更新

--- src/php/class/Response.php
+++ src/php/class/Response.php
@@ -2,27 +2,101 @@
 
 class Response
 {
+	
 	/*
-	* 返回json 格式数据
-	* param string $code  状态码
-	* param string $message 状态消息
-	* param   $data 数据
+	* 根据方式输出相应格式 
+	* param intger 相应码
+	* param string 消息
+	* param array  内容
 	*/
-	public static function json($code, $message, $data)
+	public static function result($code, $message='', $data=array())
 	{
-		if(!$code)
-		{			
+		$type = $_GET['format'];
+		if(!$type)
+		{
+			//var_dump($type);
+			$type = $_POST['format'];
+		}
+		if($type == 'json')
+		{
+			return self::json($code, $message, $data);
+		}
+		elseif($type == 'xml')
+		{
+			return self::xml($code, $message, $data);
+		}
+		elseif($type == 'array')
+		{
+			var_dump($data);
+		}
+		else{
+			
+			return self::json($code, $message, $data);
+		}
+	}
+	/*
+	* 输出json 格式 相应
+	* param intger 相应码
+	* param string 消息
+	* param array  内容
+	*/
+	public static function json($code, $message='', $data=array())
+	{
+		
+		if(!is_numeric($code))
+		{
 			return '';
 		}
 		$result = array(
 			'code'=>$code,
 			'message' => $message,
-			'data' => $data
+			'data' => $data,
 		);
+		//var_dump($result);
 		return json_encode($result);
 	}
+	/*
+	* 输出xml 格式 相应
+	* param intger 相应码
+	* param string 消息
+	* param array  内容
+	*/
+	public static function xml($code, $message='', $data=array())
+	{
+		if(!is_numeric($code))
+		{
+			return '';
+		}
+		$result = array(
+			'code'=>$code,
+			'message' => $message,
+			'data' => $data,
+		);
+		header("Content-Type:text/xml");
+		$xml = "<?xml version='1.0' encoding= 'UTF-8'?>\n";
+		$xml .= "<root>\n";
+		$xml .= self::makeItem($result);		
+		$xml .="</root>";
+		
+		return $xml;
+	}
 	
-	
-	
+	public static function makeItem($data)
+	{
+		$xml ='';
+		foreach( $data as $key => $value)
+		{
+			if(is_numeric($key))
+			{
+				$attr = " id='{$key}'";
+				$key = "item";
+			}
+			
+			$xml .= "<{$key}{$attr}>";
+			$xml .= is_array($value)? self::makeItem($value):$value;
+			$xml .= "</{$key}>\n";
+		}
+		return $xml;
+	}
 }
 ?>
--- src/php/class/myfirst.php
+++ src/php/class/myfirst.php
@@ -3,16 +3,16 @@
 //echo '第一个php 程序';
 require_once('./Response.php');
 
-//define(DB_HOST,'mysql13.000webhost.com');
-//define(DB_NAME, 'a3234098_root');
-//define(DB_PWD, '123456');
-//define(DB_DBNAME, 'a3234098_seven');
-//define(DB_TABLENAM, 'user');
-define(DB_HOST,'localhost');
-define(DB_NAME, 'root');
-define(DB_PWD, 'Alex_owen0');
-define(DB_DBNAME, 'persion');
-define(DB_TABLENAME, 'persioninfo');
+define(DB_HOST,'mysql13.000webhost.com');
+define(DB_NAME, 'a3234098_root');
+define(DB_PWD, '123456');
+define(DB_DBNAME, 'a3234098_seven');
+define(DB_TABLENAM, 'user');
+//define(DB_HOST,'localhost');
+//define(DB_NAME, 'root');
+//define(DB_PWD, 'Alex_owen0');
+//define(DB_DBNAME, 'persion');
+//define(DB_TABLENAME, 'persioninfo');
 
 //创建连接
 $conn = mysql_connect(DB_HOST,DB_NAME, DB_PWD)or die('数据库连接失败：'.mysql_error());
@@ -29,7 +29,9 @@ $result = mysql_query($query, $conn)or die('查询出错：'.mysql_error());
 //输出资源
 //print_r(mysql_fetch_array($result, MYSQL_ASSOC));
 //echo json_encode(mysql_fetch_array($result, MYSQL_ASSOC));
- echo Response::json(200, '查询成功', mysql_fetch_array ($result, MYSQL_ASSOC));
+
+ //echo Response::json(200, '查询成功', mysql_fetch_array ($result, MYSQL_ASSOC));
+ echo Response::xml(200, '查询成功', mysql_fetch_array ($result, MYSQL_ASSOC));
 //释放内存资源
 mysql_free_result($result);
 //关闭数据了连接
