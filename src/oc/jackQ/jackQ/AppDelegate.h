5a4b208d763ded2f14996dd4444d1baf488bcd68
quanbowei007 <quanbowei007@yahoo.cm>
2016-05-03 14:27:32 +0800

    create JackQ
    base init

---
+++ src/oc/jackQ/jackQ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jackQ
+//
+//  Created by owen on 16/5/3.
+//  Copyright © 2016年 simon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ src/oc/jackQ/jackQ/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  jackQ
+//
+//  Created by owen on 16/5/3.
+//  Copyright © 2016年 simon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
